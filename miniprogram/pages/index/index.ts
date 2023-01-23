33ebaed17e918d95ad4e6b85a1ffd05fa5f6e923
hasdsd <1259305706@qq.com>
2023-01-23 22:07:57 +0800

    登录页完成

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -4,46 +4,46 @@ const app = getApp<IAppOption>()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs',
-    })
+    username: '',
+    score: 0
   },
   onLoad() {
-    // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile() {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    console.log('==========onLoad==========')
+    wx.getStorage({
+      key: 'username',
+      success(res: any) {
+        if (res == null) {
+          wx.showToast({
+            title: '获取信息失败',
+            icon: 'error',
+            duration: 1000
+          })
+        } else {
+          wx.showToast({
+            title: '获取信息成功',
+            icon: 'success',
+            duration: 1000
+          })
+        }
+      },
+      fail(res: any) {
+        wx.showToast({
+          title: '获取信息失败',
+          icon: 'error',
+          duration: 1000
+        })
+        wx.navigateTo({
+          url:'../login/login'
         })
       }
     })
   },
-  getUserInfo(e: any) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  clickBtn: function (e: Function) {
+    wx.showToast({
+      title: '操作成功',
+      icon: 'success',
+      duration: 1000
     })
+
   }
 })
