dffb03b5f54c172ba8792af6189c66eef1da6976
hasdsd <1259305706@qq.com>
2023-01-24 09:57:26 +0800

    Login Code

--- miniprogram/pages/login/login.ts
+++ miniprogram/pages/login/login.ts
@@ -1,13 +1,39 @@
 // pages/login/login.ts
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    username:'',
+    password:''
+  },
+  //获取前端值
+  getUserName:function(e:any){
+    this.setData({
+      username : e.detail.value
+    })
+  },
+  getPassword:function(e:any){
+    this.setData({
+      password:e.detail.value
+    })
+  },
+  //登录
+  handleLogin: function(e:any){
+    if (this.data.username=='') {
+      wx.showModal({
+        title:'提示',
+        content:'用户名不能为空',
+        showCancel:false
+      })
+    }else{
+      
+    }
+  },
+  //注册
+  handeleRegister:function(){
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
